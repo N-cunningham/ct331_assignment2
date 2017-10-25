d20a58fc67c1c704713f8583062c832c49769ef0
Niall Cunningham <nccunningham@hotmail.com>
2017-10-25 19:55:06 +0100

    More Q2

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -1,19 +1,32 @@
 #lang racket
 
-;This is an example implementation of ins_beg,
-;It obviously doesn't do what it should, so you
-;can edit this function to get started.
-;
-;Please note the provide function is necessary
-;for the unit tests to work. Please include a
-;(provide) for each function you write in your
-;submitted assignment.
-;
-;You may delete these comments!
 
 (provide ins_beg)
+(provide ins_end)
+(provide cout_top_level)
+
+
+;A
 
 (define (ins_beg el lst)
-  (display "Running\n")
-  (cons el lst)  
+  
+  (cons el lst)
+  
+)
+
+;B
+
+(define (ins_end el lst)
+
+  (append lst (list el))
+
+)
+
+;C
+
+(define (cout_top_level list)
+  (cond [(null? list) 0]
+      
+      [else (+ 1 (cout_top_level (cdr list)))]
+  )
 )
