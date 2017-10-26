fa32b718f986a35818afba57bd234c3cc3492e80
Niall Cunningham <nccunningham@hotmail.com>
2017-10-26 15:51:05 +0100

    Question 2

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -4,6 +4,12 @@
 (provide ins_beg)
 (provide ins_end)
 (provide cout_top_level)
+(provide count_instances)
+(provide count_instances_tr)
+(provide count_instances_trH)
+(provide count_instances_deep)
+
+
 
 
 ;A
@@ -30,3 +36,75 @@
       [else (+ 1 (cout_top_level (cdr list)))]
   )
 )
+
+;D
+(define (count_instances ato list)
+  
+   (cond ((null? list)0)
+         
+   ((= ato (car list))(+ 1(count_instances ato (cdr list))))
+      
+    ((count_instances ato (cdr list)))
+    
+   )
+  
+  )
+
+;E
+
+(define (count_instances_trH ato list)
+
+  (count_instances_tr ato list 0)
+  
+)
+
+
+(define (count_instances_tr ato list total)
+  
+   (cond ((null? list) total)
+         
+   [(= ato (car list)) (count_instances_tr ato (cdr list) (+ total 1))]
+   [else (count_instances_tr ato (cdr list) total)  ]
+    
+    )
+   )
+  
+
+
+
+;F
+
+(define (count_instances_deep ato list)
+  
+  (cond [(empty? list) 0]
+        
+        [(list? (car list)) (+ (count_instances_deep ato (car list)) (count_instances_deep ato (cdr list))) ]
+     
+        [(equal? ato (car list)) (+  1 (count_instances_deep ato (cdr list)))]
+        
+        [else (count_instances_deep ato (cdr list))]
+
+        )
+
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
